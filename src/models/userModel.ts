95f205331cd36334622cb274edaefa8e8fdee704
DLionAppsPersonal <63153805+DLionApps@users.noreply.github.com>
2020-10-11 09:46:22 +0530

    Removed unwanted controller, model

--- src/models/userModel.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  name: String,
-  password: String,
-});
-
-export const User = mongoose.model("user", UserSchema);
